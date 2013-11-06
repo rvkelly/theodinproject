41ad95700ab3df3bc882b07d63107439778f490a
Paul Dariye <paul.dariye@gmail.com>
2013-11-06 01:07:18 -0600

    rails generate new

---
+++ blogger/README.md
@@ -0,0 +1,2 @@
+jumpstart's blogger
+==
---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'dd01fe047862c87e4e6fd343e8fab09f9f9e1ac6e0306b4441452074c6d9228139894b02c49718a9c282813ef0d5530cc587fdf3ad709fe9c592fc5ca85987ea'
